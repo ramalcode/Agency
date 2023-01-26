be3fdb64a0415c65a4ba685c164f543956544cb4
ramalfatullayevh <tu7ma9clt@code.edu.az>
2023-01-26 04:11:32 +0400

    Agency

---
+++ AgencyTemplate/AgencyTemplate/Areas/Manage/Controllers/PositionController.cs
@@ -0,0 +1,84 @@
+﻿using AgencyTemplate.DAL;
+using AgencyTemplate.Models;
+using AgencyTemplate.Utilies.Enums;
+using AgencyTemplate.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AgencyTemplate.Areas.Manage.Controllers
+{
+    [Area("Manage")]
+    [Authorize]
+
+    public class PositionController : Controller
+    {
+        AppDBContext _context;
+
+        public PositionController(AppDBContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index(int page=1)
+        {
+            ViewBag.MaxPageCount = Math.Ceiling((decimal)_context.Positions.Count() / 2);
+            ViewBag.CurrentPage=page;
+            return View(_context.Positions.Skip((page-1)*2).Take(2).ToList());
+        }
+
+        public async Task<IActionResult> Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CreatePositionVM positionVM)
+        {
+            if (!ModelState.IsValid) return View(positionVM);
+            if (positionVM is null) return NotFound();
+            Position position = new Position
+            {
+                Name = positionVM.Name,
+            };
+            await _context.Positions.AddAsync(position);
+            _context.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id is null) return NotFound();
+            Position position = await _context.Positions.FindAsync(id);
+            if (position is null) return NotFound();
+            _context.Positions.Remove(position);
+            _context.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+
+        public async Task<IActionResult> Update(int? id)
+        {
+            if (id is null) return NotFound();
+            Position position = await _context.Positions.FindAsync(id);
+            if (position is null) return NotFound();
+            UpdatePositionVM vM = new UpdatePositionVM
+            {
+                Name = position.Name,
+            };
+
+            return View(vM);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Update(int? id, UpdatePositionVM updatePosition)
+        {
+            if (id is null || id!=updatePosition.Id) return NotFound();
+            Position position = await _context.Positions.FindAsync(id);
+            if (position is null) return NotFound();
+            position.Name = updatePosition.Name;
+            _context.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+
+
+    }
+}
---
+++ AgencyTemplate/AgencyTemplate/Utilies/FileExtension.cs
@@ -0,0 +1,55 @@
+﻿namespace AgencyTemplate.Utilies
+{
+    public static class FileExtension
+    {
+        public static bool CheckType(this IFormFile file, string type) => file.ContentType.Contains(type);
+        public static bool CheckSize(this IFormFile file, int kb) => kb * 1024 > file.Length;
+
+        static string ChangeFileName(string oldName)
+        {
+            string extension = oldName.Substring(oldName.IndexOf('.'));
+            if(oldName.Length < 32)
+            {
+                oldName = oldName.Substring(0,oldName.IndexOf('.'));
+            }
+            else
+            {
+                oldName = oldName.Substring(0, 31);
+            }
+            string newName = Guid.NewGuid()+oldName+extension;
+            return newName;
+        }
+
+        public static string SaveFile(this IFormFile file, string path)
+        {
+            string fileName = ChangeFileName(file.FileName);
+            using (FileStream fs = new FileStream(Path.Combine(path,fileName) ,FileMode.Create))
+            {
+                file.CopyTo(fs);
+            }
+            return fileName;
+        }
+
+        public static void DeleteFile(this string file, string root, string folder)
+        {
+            string path = Path.Combine(root, folder, file);
+            if (File.Exists(path))
+            {
+                File.Delete(path);
+            }
+        }
+        public static string CheckValidate(this IFormFile file, string type, int kb)
+        {
+            string result = "";
+            if (!file.CheckType(type))
+            {
+                result += "Type is wrong";
+            }
+            if (!file.CheckSize(kb))
+            {
+                result += "Size is wrong";
+            }
+            return result;
+        }
+    }
+}
